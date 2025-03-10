51534ec6c6c0842b1b7e1bc5340d1fab9b882495
JAfifah <jamiatul.afifah2735@gmail.com>
2025-03-10 13:07:01 +0700

    Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,22 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserModel;
-use Illuminate\Support\Facades\Hash; // Tambahkan ini
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
-{
-    $user = UserModel::firstOrNew([
-        'username' => 'manager33'
-    ], [
-        'nama' => 'Manager Tiga Tiga',
-        'password' => Hash::make('12345'),
-        'level_id' => 2
-    ]);
+    {
+        // Membuat user baru
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager 11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
 
-    $user->save(); // Simpan data ke database
+        // Mengubah username
+        $user->username = 'manager12';
 
-    return view('user', ['data' => $user]);
-}
+        // Menyimpan perubahan
+        $user->save();
+
+        // Mengecek perubahan dengan wasChanged()
+        $wasChanged = $user->wasChanged(); // true
+        $wasChangedUsername = $user->wasChanged('username'); // true
+        $wasChangedMulti = $user->wasChanged(['username', 'level_id']); // true
+        $wasChangedName = $user->wasChanged('name'); // false
+        $wasChangedMultiCheck = $user->wasChanged(['name', 'username']); // true
+
+        // Debugging output
+        dd([
+            'wasChanged' => $wasChanged,
+            'wasChangedUsername' => $wasChangedUsername,
+            'wasChangedMulti' => $wasChangedMulti,
+            'wasChangedName' => $wasChangedName,
+            'wasChangedMultiCheck' => $wasChangedMultiCheck,
+        ]);
+    }
 }
