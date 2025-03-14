b50598827e8b6c8bd4e46c7b72cca26ee28ab3d0
JAfifah <jamiatul.afifah2735@gmail.com>
2025-03-14 13:56:38 +0700

    praktikum 2 1-14

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/kategori', [KategoriController::class, 'index']);
+
+
 Route::get('/', function () {
     return view('welcome');
 });
