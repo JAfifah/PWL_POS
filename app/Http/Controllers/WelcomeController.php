93e40fe5e577ff19afad099990d26f5bdde02dec
JAfifah <jamiatul.afifah2735@gmail.com>
2025-03-24 14:38:46 +0700

    JS5 REV PRAKTIKUM2

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function index()
+    {
+        return view('welcome', [
+            'activeMenu' => 'dashboard',
+            'breadcrumb' => (object)[
+                'title' => 'Selamat Datang',
+                'list' => ['Home', 'Welcome']
+            ]
+        ]);
+    }
+}
